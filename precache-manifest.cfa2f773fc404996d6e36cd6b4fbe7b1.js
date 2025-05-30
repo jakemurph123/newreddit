f47cbea2943593413da7e5ce3d0491afa238d85c
Jacob <jakemurph500@gmail.com>
2025-05-29 20:48:32 -0400

    Updates

---
+++ precache-manifest.cfa2f773fc404996d6e36cd6b4fbe7b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22125eaae6490f068e49087252f29c3d",
+    "url": "/newreddit/index.html"
+  },
+  {
+    "revision": "f72c12b5860a8662f908",
+    "url": "/newreddit/static/css/main.791fefe6.chunk.css"
+  },
+  {
+    "revision": "c261bd2e2220653548e0",
+    "url": "/newreddit/static/js/2.b5ad368a.chunk.js"
+  },
+  {
+    "revision": "f09bafd8b2e7fe4d8e763cfbd1fdf23b",
+    "url": "/newreddit/static/js/2.b5ad368a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f72c12b5860a8662f908",
+    "url": "/newreddit/static/js/main.14bb4221.chunk.js"
+  },
+  {
+    "revision": "613e74cc67ee061e5e85",
+    "url": "/newreddit/static/js/runtime-main.085f3e4d.js"
+  }
+]);
